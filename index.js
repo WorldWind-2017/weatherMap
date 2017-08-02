9e11e5954f38abdabbe6cdbe83d33d47c9eb30c0
Atreya Iyer <atreya.iyer@menloschool.org>
2017-08-02 15:05:35 -0700

    added getAPIURL

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
  * Created by atreyaiyer on 8/2/17.
  */
 var APIKey = 'babeba397663d3ffbdb30c76c2a41181';
+function createAPIURL(lat, long)
+{
+    return 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + APIKey;
+}
+
