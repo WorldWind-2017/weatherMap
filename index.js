cac72a71e1af86eb92a3c0f6d1efdcb7de0057bd
Atreya Iyer <atreya.iyer@menloschool.org>
2017-08-02 15:33:24 -0700

    added imports to html file

--- index.js
+++ index.js
@@ -16,41 +16,39 @@ requirejs({paths:{
         './LayerManager', 'src/formats/kml/KmlFile',
         'src/formats/kml/controls/KmlTreeVisibility', './Pin', 'jquery'],
     function (ww,
-              LayerManager, KmlFile, KmlTreeVisibility)
-    {
+              LayerManager, KmlFile, KmlTreeVisibility) {
         "use strict";
-    }
 
-    //returns API URL as a string
-    function createAPIURL(lat, long)
-    {
-        return 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + APIKey;
-    }
 
-    //returns weather formatted as an HTML String
-    function formatWeather(APIURL)
-    {
-        $.ajax({
-            url: encodeURI(APIURL),
-            method: 'get',
-            dataType: 'json',
-            success: function(data) {
-                //Create some html
-                var tempHTML = '<h5 class="fontsize"><b>Weather Details for ' + data.name + '</b></h5>';
-                tempHTML += '<p><b>Country:</b> ' + data.sys.country + '</p><br>';
-                tempHTML += '<p><b>Current Outlook:</b> ' + data.weather[0].main + '</p><br>';
-                tempHTML += '<p><b>Current Outlook Description:</b> ' + data.weather[0].description + '</p><br>';
-                tempHTML += '<p><b>Current Temperature (Celsius):</b> ' + Math.round((data.main.temp - 272),2) + '</p><br>';
-                tempHTML += '<p><b>Sunrise:</b> ' + timeConverter(data.sys.sunrise) + '</p><br>';
-                tempHTML += '<p><b>Sunset:</b> ' + timeConverter(data.sys.sunset) + '</p><br>';
-                tempHTML += '<p><b>Max Temperature Today (Celsius):</b> ' + Math.round((data.main.temp_max - 272),2) + '</p><br>';
-                tempHTML += '<p><b>Min Temperature Today (Celsius):</b> ' + Math.round(data.main.temp_min  - 272, 2) + '</p><br>';
-                tempHTML += '<p><b>Pressure (HPa):</b> ' + data.main.pressure + '</p><br>';
-                tempHTML += '<p><b>Humidity (%):</b> ' + data.main.humidity + '</p><br>';
-                tempHTML += '<p><b>Wind speed (m/s):</b>' + data.wind.speed + '</p><br><br>';
-            },
-            fail: function() {
+        //returns API URL as a string
+        function createAPIURL(lat, long) {
+            return 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + '&appid=' + APIKey;
+        }
 
-            }
-        })
+        //returns weather formatted as an HTML String
+        function formatWeather(APIURL) {
+            $.ajax({
+                url: encodeURI(APIURL),
+                method: 'get',
+                dataType: 'json',
+                success: function (data) {
+                    //Create some html
+                    var tempHTML = '<h5 class="fontsize"><b>Weather Details for ' + data.name + '</b></h5>';
+                    tempHTML += '<p><b>Country:</b> ' + data.sys.country + '</p><br>';
+                    tempHTML += '<p><b>Current Outlook:</b> ' + data.weather[0].main + '</p><br>';
+                    tempHTML += '<p><b>Current Outlook Description:</b> ' + data.weather[0].description + '</p><br>';
+                    tempHTML += '<p><b>Current Temperature (Celsius):</b> ' + Math.round((data.main.temp - 272), 2) + '</p><br>';
+                    tempHTML += '<p><b>Sunrise:</b> ' + timeConverter(data.sys.sunrise) + '</p><br>';
+                    tempHTML += '<p><b>Sunset:</b> ' + timeConverter(data.sys.sunset) + '</p><br>';
+                    tempHTML += '<p><b>Max Temperature Today (Celsius):</b> ' + Math.round((data.main.temp_max - 272), 2) + '</p><br>';
+                    tempHTML += '<p><b>Min Temperature Today (Celsius):</b> ' + Math.round(data.main.temp_min - 272, 2) + '</p><br>';
+                    tempHTML += '<p><b>Pressure (HPa):</b> ' + data.main.pressure + '</p><br>';
+                    tempHTML += '<p><b>Humidity (%):</b> ' + data.main.humidity + '</p><br>';
+                    tempHTML += '<p><b>Wind speed (m/s):</b>' + data.wind.speed + '</p><br><br>';
+                },
+                fail: function () {
+
+                }
+            })
+        }
     }
